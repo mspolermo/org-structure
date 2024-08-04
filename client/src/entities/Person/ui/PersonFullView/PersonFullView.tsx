5b7a1759db71f90fb672dbddebcf7fa25d7a77fe
mspolermo <mspolermo@gmail.com>
2024-08-04 16:32:29 +0500

    Client: PersonFullView fixed

--- client/src/entities/Person/ui/PersonFullView/PersonFullView.tsx
+++ client/src/entities/Person/ui/PersonFullView/PersonFullView.tsx
@@ -48,102 +48,107 @@ export const PersonFullView = observer(({ className, person, personDetales }: Pe
     )
 
     const PersonDetalesBlock = () => (
-        <VStack gap="8" max className={cls.block}>
-            <Text title="Детализация сотрудника" size="xl"/>
-            <HStack gap="8">
-                <Text text={'Оборудование:'} thin/>
-                <Text text={personDetales.items} thin/>
-            </HStack>
-            <HStack gap="8">
-                <Text text={'Техника:'} thin/>
-                <Text text={personDetales.hardware} thin/>
-            </HStack>
-            <HStack gap="8">
-                <Text text={'Програмное обеспечение:'} thin/>
-                <Text text={personDetales.software} thin/>
-            </HStack>
-            <HStack gap="8">
-                <Text text={'Пройденные курсы:'} thin/>
-                <Text text={personDetales.exams} thin/>
-            </HStack>
-        </VStack>
-    )
-
-    return (
-        <Card>
-            <VStack gap="16" max className={classNames(cls.PersonFullView, {}, [className])}>
-                <ServiceBlock />
-                <HStack max gap='32' className={cls.block}>
-                    <VStack 
-                        align='center'
-                        justify='center'
-                        style={{'backgroundColor': getColor(person.id)}}
-                        className={cls.photo}
-                    >
-                        {getInitials(person.name)}
-                    </VStack>
-                
-                    <VStack gap="8" max>
-                        <Text title="Общая информация" size="xl"/>
-                    
-                        <HStack gap="16" max>
-                            <Text text={'Имя'} thin/>
-                            <Input inputVariant="clear" className={cls.input} placeholder="ФИО сотрудника" value={person.name}/>
-                        </HStack>
-                        <HStack gap="4" max>
-                            <Text text="Телефон:" thin/>
-                            <Input inputVariant="clear" className={cls.input} placeholder="Номер телефона" value={person.phone}/>
-                        </HStack>
-                        <HStack gap="4" max>
-                            <Text title="Расположение" thin/>
-                            <Input inputVariant="clear" className={cls.input} placeholder="Расположение" value={person.location}/>
-                        </HStack>
-                        <HStack gap="4" max>
-                            <Text title="Должность" thin/>
-                            <Input inputVariant="clear" className={cls.input} placeholder="Должность" value={person.post}/>
-                        </HStack>
-                        <HStack gap="4" max>
-                            <Text title="Email" thin/>
-                            <Input inputVariant="clear" className={cls.input} placeholder="Почтовый адрес" value={person.email}/>
-                        </HStack>
-                        <HStack gap="4" max>
-                            <Text title="Дата рождения" thin />
-                            <Input
-                                inputVariant="clear"
-                                className={cls.input}
-                                placeholder="Должность"
-                                value={formatDate(person.birthday.toString())}
-                            />
-                        </HStack>
-                    </VStack>
+        <Card border='border-slightly' padding='16' max>
+            <VStack gap="8" max className={cls.block}>
+                <Text title="Детализация сотрудника" size="xl"/>
+                <HStack gap="8">
+                    <Text text={'Оборудование:'} thin/>
+                    <Text text={personDetales.items} thin/>
                 </HStack>
-
-                <VStack className={cls.block}>
-                    <VStack gap="8" >
-                        <Text title="Дополнительные настройки" size="xl"/>
-                        <Toggle 
-                            label="Начальник отдела" 
-                            value={person.isChef}
-                            onChange={(e) => (console.log('photoChanged: ' + e))}
-                        />
-                    
-                        <Toggle 
-                            label="Менеджер отдела" 
-                            value={person.isManager}
-                            onChange={(e) => (console.log('photoChanged: ' + e))}
-                        />
-                    </VStack>
-                </VStack>
-
-                <PersonDetalesBlock />
-            
-                <HStack justify="end" align="center" gap="16" max>
-                    <Button disabled>Сохранить</Button>
-                    <Button onClick={onReturnHandler}>
-                        Отмена
-                    </Button>
+                <HStack gap="8">
+                    <Text text={'Техника:'} thin/>
+                    <Text text={personDetales.hardware} thin/>
+                </HStack>
+                <HStack gap="8">
+                    <Text text={'Програмное обеспечение:'} thin/>
+                    <Text text={personDetales.software} thin/>
+                </HStack>
+                <HStack gap="8">
+                    <Text text={'Пройденные курсы:'} thin/>
+                    <Text text={personDetales.exams} thin/>
                 </HStack>
             </VStack>
         </Card>
+    )
+
+    const PersonBlock = () => (
+        <Card border='border-slightly' padding='16' max>
+            <HStack max gap='32' className={cls.block}>
+                <VStack 
+                    align='center'
+                    justify='center'
+                    style={{'backgroundColor': getColor(person.id)}}
+                    className={cls.photo}
+                >
+                    {getInitials(person.name)}
+                </VStack>
+                
+                <VStack gap="8" max>
+                    <Text title="Общая информация" size="xl"/>
+                    
+                    <HStack gap="16" max>
+                        <Text text={'Имя'} thin/>
+                        <Input inputVariant="clear" className={cls.input} placeholder="ФИО сотрудника" value={person.name}/>
+                    </HStack>
+                    <HStack gap="4" max>
+                        <Text text="Телефон:" thin/>
+                        <Input inputVariant="clear" className={cls.input} placeholder="Номер телефона" value={person.phone}/>
+                    </HStack>
+                    <HStack gap="4" max>
+                        <Text title="Расположение" thin/>
+                        <Input inputVariant="clear" className={cls.input} placeholder="Расположение" value={person.location}/>
+                    </HStack>
+                    <HStack gap="4" max>
+                        <Text title="Должность" thin/>
+                        <Input inputVariant="clear" className={cls.input} placeholder="Должность" value={person.post}/>
+                    </HStack>
+                    <HStack gap="4" max>
+                        <Text title="Email" thin/>
+                        <Input inputVariant="clear" className={cls.input} placeholder="Почтовый адрес" value={person.email}/>
+                    </HStack>
+                    <HStack gap="4" max>
+                        <Text title="Дата рождения" thin />
+                        <Input
+                            inputVariant="clear"
+                            className={cls.input}
+                            placeholder="Должность"
+                            value={formatDate(person.birthday.toString())}
+                        />
+                    </HStack>
+                </VStack>
+            </HStack>
+
+            <VStack className={cls.block}>
+                <VStack gap="8" >
+                    <Text title="Дополнительные настройки" size="xl"/>
+                    <Toggle 
+                        label="Начальник отдела" 
+                        value={person.isChef}
+                        onChange={(e) => (console.log('photoChanged: ' + e))}
+                    />
+                    
+                    <Toggle 
+                        label="Менеджер отдела" 
+                        value={person.isManager}
+                        onChange={(e) => (console.log('photoChanged: ' + e))}
+                    />
+                </VStack>
+            </VStack>
+        </Card>
+    )
+
+    return (
+        <VStack gap="16" max className={classNames(cls.PersonFullView, {}, [className])}>
+            <ServiceBlock />
+            <PersonBlock />
+            <PersonDetalesBlock />
+            
+            <HStack justify="end" align="center" gap="16" max>
+                <Button disabled>Сохранить</Button>
+                <Button onClick={onReturnHandler}>
+                    Отмена
+                </Button>
+            </HStack>
+        </VStack>
     );
 });
