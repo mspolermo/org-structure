1d46dcbe1c987d98e71a4e6b49b40b0b785ffbad
mspolermo <mspolermo@gmail.com>
2024-11-09 12:10:18 +0500

    Client: start Auth page logic

--- client/src/pages/AuthorizationPage/ui/AuthorizationPage.tsx
+++ client/src/pages/AuthorizationPage/ui/AuthorizationPage.tsx
@@ -1,15 +1,50 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 
-import { VStack } from "@/shared/ui/Stack";
+import { Button } from "@/shared/ui/Button";
+import { Card } from "@/shared/ui/Card";
+import { Input } from "@/shared/ui/Input";
+import { HStack, VStack } from "@/shared/ui/Stack";
 import { Text } from "@/shared/ui/Text";
 import { Page } from "@/widgets/Page";
 
 const AuthorizationPage = memo(() => {
+
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <Page>
-            <VStack max maxHeight align="center" justify="center">
-                <Text title="Страница авторизации" />
-            </VStack>
+        <Page header="Страница авторизации">
+            <Card border='border-slightly' padding='24' max>
+                <VStack gap="16" max>
+
+                    <Text title="Пожалуйста авторизируйтесь" />
+
+                    <HStack gap="16" max>
+                        <Text title={'Email:'} thin />
+                        <Input 
+                            inputVariant="bordered"
+                            placeholder="Введите email"
+                            value={login}
+                            onChange={setLogin}
+                        />
+                    </HStack>
+
+                    <HStack gap="16" max>
+                        <Text title={'Пароль:'} thin />
+                        <Input 
+                            inputVariant="bordered"
+                            placeholder="Введите пароль"
+                            value={password}
+                            onChange={setPassword}
+                        />
+                    </HStack>
+
+                    <Button>Войти</Button>
+                </VStack>
+            </Card>
+
+
+
         </Page>
     );
 });
